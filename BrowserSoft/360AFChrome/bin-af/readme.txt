4195893fc163b4bd06d361625544ec6160f960ff
Okmeque1 <okmeque1@gmail.com>
2024-02-28 17:59:18 +0000

    test 360afchrome

---
+++ BrowserSoft/360AFChrome/bin-af/readme.txt
@@ -0,0 +1,3 @@
+360Loader.exe   is the default browser for this PC
+360af_9.exe  is the browser loaded by JD2, also can be used as Px browser
+
---
+++ BrowserSoft/360AFChrome/bin-af5/360Loader.bat
@@ -0,0 +1,2 @@
+start "" "%~dp0\360af5chrome.exe" --user-data-dir=%~dp0\360af5UData1 --disable-infobars --no-sandbox --disable-logging --no-default-browser-check --disable-component-update --disable-background-networking --allow-outdated-plugins --cipher-suite-blacklist=0xcc14,0xe013  --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+exit /b
